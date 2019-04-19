8ee7c525ab372f334c19b3db72347641984300a2
creiser2 <carlreiser4@gmail.com>
2019-04-19 16:42:10 -0400

    geofencing

--- src/components/screens/MapScreen.js
+++ src/components/screens/MapScreen.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Permissions, Location, MapView } from 'expo';
+import { Permissions, Location, MapView, TaskManager } from 'expo';
 import { connect } from 'react-redux';
 import { SafeAreaView } from 'react-native';
 import {DAY_MAP_STYLE, NIGHT_MAP_STYLE} from '../../../constants/mapstyles';
@@ -16,39 +16,102 @@ import {
 } from 'react-native'
 
 
+type Position = {
+  coords: {
+    accuracy: number,
+    altitude: number,
+      altitudeAccuracy: number,
+      heading: number,
+      latitude: number,
+      longitude: number,
+      speed: number,
+    },
+    timestamp: number,
+};
+
+      
+
+
 class MapScreen extends Component {
+  //this provides us with a reference to our mapview object in code
+  map = React.createRef();
+
   state = {
     ready: false,
     longitude: null,
     latitude: null,
     region: "",
     city: "",
-  }  
+  };  
 
+
+  //to remove our watcher, for now we do not want to keep watching location after the component unmounts, but this will change later
+  watcher: { remove: () => void };
 
   async componentDidMount() {
     const { status } = await Permissions.askAsync(Permissions.LOCATION);
     if (status === 'granted') {
+
+      //get initial location to render in map, this will be later updated by our watcher
       const { coords: { latitude, longitude } } = await Location.getCurrentPositionAsync();
       //get city and state
       const strLoc =  await Location.reverseGeocodeAsync({ latitude, longitude })
       this.setState({ ready: true, city: strLoc[0].city, region: strLoc[0].region, longitude, latitude });
+
+      //start watching position
+      const options = { enableHighAccuracy: true, timeInterval: 1000, distanceInterval: 1 };
+
+      //the onNewPosition function will be called for each new position captured after 1 second
+      this.watcher = await Location.watchPositionAsync(options, this.onNewPosition);
+      
     } else {
       alert("We couldn't get your location");
       this.props.destroyUser();
     }
   }
 
-  componentWillUnmount = () => {
-    
+  //destroy watch position
+  componentWillUnmount() {
+    this.watcher.remove();
+  }
+
+ 
+
+  //called when we physically move on map
+  onNewPosition = (position: Position) => {
+    //animate the map to track your movements away from mapview
+    this.map.current.animateToCoordinate(position.coords, 1000);
+    //set the state of the application
+    this.setState({
+      latitude: position.coords.latitude,
+      longitude: position.coords.longitude
+    })
   }
 
 
+  //this could contain bugs, we are setting the redux state of our user to initial state if they click back button
   handleBackButtonClicked = () => {
     this.props.destroyUser();
     this.props.navigation.navigate('HomeScreen');
   }
 
+  onPress = async () => {
+    //begin geofencing based on current regions
+    //These regions will be stored in redux, and we will fetch them with an action such as "GET_REGIONS"
+    //For now we will apply this to our test region
+    const regions = [
+      {
+        latitude: 45.886834, 
+        longitude: 5.124,
+        radius: 100,
+        notifyOnEnter: true,
+        notifyOnExit: true
+      }
+    ]
+
+    // //we are basically checking to see if our location is in one of the above regions
+    await Location.startGeofencingAsync("HANDLE_REGION", regions)
+  }
 
 
   render() {
@@ -65,14 +128,16 @@ class MapScreen extends Component {
           <View style={styles.container}>
             <StatusBar backgroundColor="blue" barStyle="light-content" />
             <View style={styles.topBar}>
-                <Text style={styles.cityText}>
+                <Text onPress={this.onPress} style={styles.cityText}>
                     {this.state.city}, {this.state.region}
                 </Text>
                 <Text style={styles.usernameText}>
                   {this.props.user.uid}
                 </Text>
             </View>
-            <MapView style={styles.map}  provider="google" customMapStyle={NIGHT_MAP_STYLE}
+            <MapView 
+                ref={this.map}
+                style={styles.map}  provider="google" customMapStyle={NIGHT_MAP_STYLE}
                 initialRegion={{
                 latitude,
                 longitude,
@@ -83,48 +148,18 @@ class MapScreen extends Component {
               showsUserLocation={true}
             > 
               <MapView.Marker
-                coordinate={{latitude: latitude - 0.002,  longitude: longitude}}
+                coordinate={{latitude: 45.886834,  longitude: longitude}}
                 title={this.props.isGuest.toString()}
                 description={"great job"}
                 opacity={0.5}
                 pinColor={"#4CFF4F"}
               />
               <MapView.Circle
-                center = {{latitude: latitude - 0.002,  longitude: longitude} }
+                center = {{latitude: 45.886834,  longitude: longitude} }
                 radius = { 100 }
                 strokeWidth = { 1 }
                 strokeColor = { '#1a66ff' }
                 fillColor = { 'rgba(63, 63, 191, 0.26)' }
-              />
-              <MapView.Marker
-                coordinate={{latitude: latitude + 0.001,
-                longitude: longitude}}
-                title={"Second list"}
-                description={"ok"}
-                opacity={1}
-                pinColor={"#000"}
-              />
-              <MapView.Circle
-                center = {{latitude: latitude + 0.001,  longitude: longitude} }
-                radius = { 100 }
-                strokeWidth = { 1 }
-                strokeColor = { '#fff' }
-                fillColor = { 'rgba(0, 0, 0, 0.68)' }
-              />
-              <MapView.Marker
-                coordinate={{latitude: latitude + 0.0005,
-                longitude: longitude  + 0.001}}
-                title={"Second list"}
-                description={"ok..."}
-                opacity={1}
-                pinColor={"#000"}
-              />
-              <MapView.Circle
-                center = {{latitude: latitude + 0.0005,  longitude: longitude + 0.001} }
-                radius = { 100 }
-                strokeWidth = { 1 }
-                strokeColor = { '#fff' }
-                fillColor = { 'rgba(0, 0, 0, 0.68)' }
               />
             </MapView>
             <View style={styles.bottomBar}>
@@ -138,6 +173,18 @@ class MapScreen extends Component {
     }
   }
 }
+
+TaskManager.defineTask("HANDLE_REGION", ({ data: { eventType, region }, error }) => {
+  if (error) {
+    // Error occurred - check `error.message` for more details.
+    return;
+  }
+	if (eventType === Location.GeofencingEventType.Enter) {
+		console.log("entered");
+	} else if (eventType === Location.GeofencingEventType.Exit) {
+	  console.log("exited");
+	}
+});
 
 //Redux setup
 
