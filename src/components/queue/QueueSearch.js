a2b46ef469c536c0e7ac52bb198ebffa163264c1
Johnny Persichilli <johnnyperch@Johnnys-MacBook-Pro.local>
2019-09-08 21:12:56 -0400

    added api search of spotify from queue search page

--- src/components/queue/QueueSearch.js
+++ src/components/queue/QueueSearch.js
@@ -3,6 +3,7 @@ import { connect } from 'react-redux';
 import { SafeAreaView } from 'react-native';
 import { AUTHORIZE_SPOTIFY, HEADERS } from '../../../constants/api/spotify';
 import { HOMESCREEN_BACKGROUND, WHITE } from '../../../constants/colors';
+import axios from 'axios';
 
 
 import {
@@ -11,27 +12,58 @@ import {
   TouchableOpacity,
   Text,
   View,
+  TextInput,
 } from 'react-native'
 
 
 class QueueSearch extends Component {
   state = {
-
+    songSearch: "",
+    searchResults: null,
   }
   componentDidMount = () => {
 
   }
+
+  searchSpotify = async () => {
+    let searchURL = `https://api.spotify.com/v1/search?q=${this.state.songSearch}&type=album,track,artist,playlist&limit=5`
+    const songResponse = await axios.get(searchURL, {
+      headers: {
+        "Authorization": `Bearer ${this.props.user.spotify_access_token}`
+      }
+    });
+    this.setState({searchResults: songResponse})
+    console.log("res:"+JSON.stringify(songResponse));
+  }
+
   render() {
-    console.log("Props token search:", this.props.user.spotify_access_token)
       return (
         <SafeAreaView style={{flex: 1, backgroundColor: HOMESCREEN_BACKGROUND}}>
           <View style={styles.container}>
             <View style={styles.topBar}>
               <Text style={styles.title}>Queue Search</Text>
+              <TextInput
+                style={styles.inputQueueNameAndSong}
+                enablesReturnKeyAutomatically={true}
+                selectTextOnFocus={true}
+                placeholder="Insert song to search for: "
+                onChangeText={(songSearch) => this.setState({songSearch})}
+                value={this.state.insertQueueName}
+              />
+              <TouchableOpacity
+                style={styles.joinButton}
+                onPress={() => this.searchSpotify()}>
+                    <Text style={styles.joinButtonText}>Search API for song</Text>
+              </TouchableOpacity>
               <TouchableOpacity
                 style={styles.joinButton}
                 onPress={() => this.props.navigation.navigate('QueueScreen')}>
                     <Text style={styles.joinButtonText}>Back to Queue Search</Text>
+              </TouchableOpacity>
+              <TouchableOpacity
+                style={styles.joinButton}
+                onPress={() => console.log(this.state.searchResults)}>
+                    <Text style={styles.joinButtonText}>Print songs</Text>
               </TouchableOpacity>
             </View>
           </View>
