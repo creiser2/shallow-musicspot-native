be4d0a1c15434671399ce07093faec9e0417c524
Johnny Persichilli <johnnyperch@Johnnys-MacBook-Pro.local>
2019-09-09 00:44:56 -0400

    parsed json results into custom song objects and basic rendering

--- src/components/queue/QueueSearch.js
+++ src/components/queue/QueueSearch.js
@@ -4,36 +4,73 @@ import { SafeAreaView } from 'react-native';
 import { AUTHORIZE_SPOTIFY, HEADERS } from '../../../constants/api/spotify';
 import { HOMESCREEN_BACKGROUND, WHITE } from '../../../constants/colors';
 import axios from 'axios';
+import Song from '../../customClasses/Song'
+import { Thumbnail } from 'native-base';
 
 
 import {
   AppRegistry,
   StyleSheet, 
   TouchableOpacity,
-  Text,
   View,
   TextInput,
+  Text,
 } from 'react-native'
 
+const defaultImage = 'https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcRQgZ48o39T52Ycjne2VfHtDdR08ftNInjPaTuwFzWyWEKdJJgg'
 
 class QueueSearch extends Component {
   state = {
     songSearch: "",
-    searchResults: null,
+    searchResults: [],
+
   }
   componentDidMount = () => {
 
   }
 
+  parseJsonToSongs = (searchResults) => {
+    tempSongArray=[];
+    searchResults.data.tracks.items.map((song) => {
+      //console.log(JSON.stringify(song));
+      if(song.images){
+        var tempSong = new Song(song.id, song.name, song.artists[0].id,song.uri,song.duration_ms,song.explicit,song.images[0].url)
+      }else{
+        var tempSong = new Song(song.id, song.name, song.artists[0].id,song.uri,song.duration_ms,song.explicit, defaultImage)
+      }
+      tempSongArray.push(tempSong)
+    });
+    this.setState({searchResults:tempSongArray})
+  }
+
   searchSpotify = async () => {
-    let searchURL = `https://api.spotify.com/v1/search?q=${this.state.songSearch}&type=album,track,artist,playlist&limit=5`
-    const songResponse = await axios.get(searchURL, {
+    let searchURL = `https://api.spotify.com/v1/search?q=${this.state.songSearch}&type=track&limit=3`
+    const searchResults = await axios.get(searchURL, {
       headers: {
         "Authorization": `Bearer ${this.props.user.spotify_access_token}`
       }
     });
-    this.setState({searchResults: songResponse})
-    console.log("res:"+JSON.stringify(songResponse));
+    this.parseJsonToSongs(searchResults)
+  }
+
+  renderSongObj = (song) => {
+    //console.log("Song:",song.name);
+    return (
+        
+      <View key={song.id} style={styles.songContainer}>
+          <View style={styles.thumbnail}>
+            <Thumbnail source={{uri: song.imageURL}} />
+          </View>
+           <View style={styles.title}>
+            <Text style={styles.titleText}>{song.name} -{song.artistId}</Text>
+          </View> 
+           <View style={styles.time}>
+            <Text style={styles.timeText}>{song.numVotes}</Text>
+           </View>
+      </View>
+    
+  
+    );
   }
 
   render() {
@@ -48,7 +85,7 @@ class QueueSearch extends Component {
                 selectTextOnFocus={true}
                 placeholder="Insert song to search for: "
                 onChangeText={(songSearch) => this.setState({songSearch})}
-                value={this.state.insertQueueName}
+                value={this.state.songSearch}
               />
               <TouchableOpacity
                 style={styles.joinButton}
@@ -65,6 +102,9 @@ class QueueSearch extends Component {
                 onPress={() => console.log(this.state.searchResults)}>
                     <Text style={styles.joinButtonText}>Print songs</Text>
               </TouchableOpacity>
+              <View>
+                {this.state.searchResults.map( (song) => this.renderSongObj(song) )}
+              </View>
             </View>
           </View>
         </SafeAreaView>
@@ -96,5 +136,35 @@ export default connect(msp, mdp)(QueueSearch)
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    backgroundColor: HOMESCREEN_BACKGROUND,
   },
+  songContainer: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    height: 100,
+    width: '100%',
+    marginLeft: 15,
+    marginRight: 15
+  },
+  thumbnail: {
+    alignSelf: "center",
+  },
+  title: {
+    alignSelf: "center",
+    width: 180
+  },
+  time: {
+    alignSelf: "center"
+  },
+  titleText: {
+    color: 'white',
+    fontSize: 16
+  },
+  timeText: {
+    color: 'white',
+    fontSize: 13,
+    opacity: 0.7,
+    alignSelf:"flex-end",
+    marginLeft:10
+  }
 });
--- src/customClasses/Song.js
+++ src/customClasses/Song.js
@@ -1,3 +1,20 @@
 export default class Song {
-    //add neccassary fields from db here
+    constructor(id, name, artistId, uri, duration_ms, explicit, imageURL) {
+        this.id = id;
+        this.name = name;
+        this.artistId = artistId;
+        this.uri = uri;
+        this.duration_ms = duration_ms;
+        this.explicit = explicit;
+        this.imageURL = imageURL;
+        this.numVotes = 0;
+    }
+
+    // Adding a method to the constructor
+    upVote() {
+        return ++this.numVotes;
+    }
+    downVote() {
+        return --this.numVotes;
+    }
 }
